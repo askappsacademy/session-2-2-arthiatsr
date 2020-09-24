694874af30f9b5f37ba0f3c004695bb77012b637
Arthi KS <arthiatsr@gmail.com>
2020-09-24 18:41:32 +0000

    Section 2 Exercise 2 finished

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const run = function () {
-    // Exercise 2
+window.onload = function(){
+  document.getElementById("ptag-id").innerHTML = "Hello";
 
-    // Exercise 3 - Extra Credit
-    const fruits = ['Apple', 'Banana', 'Orange'];
+  const run = function () {  
     
-    // hint 1: how do we loop through items in JavaScript
+      // Exercise 2
+      var x = document.getElementById("ptag-id");
+      x.style.color = "red";
 
-    // hint 2: use how do we create an element and append an element?
+      // Exercise 3 - Extra Credit
+      const fruits = ['Apple', 'Banana', 'Orange'];
+      var newElement1 = document.createElement("div");
+      var newElement2 = document.createElement("ul");
+      x.appendChild(newElement1);
+      newElement1.appendChild(newElement2);
+      var liHead = document.createElement("li");      
+      newElement2.appendChild(liHead);
+      liHead.innerHTML = "Fruit Cart";      
+      // hint 1: how do we loop through items in JavaScript
+      for(var i=0; i < fruits.length; i++){
+        var liList = document.createElement("li");
+        liList.innerHTML = fruits[i];
+        liHead.appendChild(liList);
+      }
+      // hint 2: use how do we create an element and append an element?
+  }
+
+  run()
+
 }
-
-run()
